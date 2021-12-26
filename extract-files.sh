8c141800533c4b2af9e462891a2cb01860e0bbb3
dianlujitao <dianlujitao@lineageos.org>
2021-12-27 00:02:00 +0800

    garnet: Drop dependencies on android.hidl.base@1.0.so
    Change-Id: I4c6803a6f1bfdd3fde7d83f59c0642f3398f531b

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        system_ext/lib64/libwfdnative.so)
+            ${PATCHELF} --remove-needed "android.hidl.base@1.0.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.security.keymint-service-qti|vendor/lib/libqtikeymint.so|vendor/lib64/libqtikeymint.so)
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || "${PATCHELF_0_17_2}" --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
