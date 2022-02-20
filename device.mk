802bbc2b6a1fc6489e2d3db12ce6c10258230dd9
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    garnet: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -233,7 +233,11 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1.vendor
+    android.hardware.sensors@2.1.vendor \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
