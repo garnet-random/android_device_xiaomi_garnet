9cdf9daa510fe116adbefb194cf85c1f79ddb948
Arian <arian.kulmer@web.de>
2022-06-18 18:55:07 +0200

    garnet: Build telephony packages and disable rild
    Change-Id: I876600dd0c72eceeef65be9d78d869368ec635c9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_F2FS := true
 
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
