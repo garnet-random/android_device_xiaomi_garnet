490b83044da3792cbf30e3a1df172a6c2a0a9f62
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-26 06:20:05 +0530

    garnet: parts: Update overrideHdrTypes API for U
    It's moved to DisplayManager in U.
    
    Change-Id: Ia5b5764a298c185cd5f25577881162e3bf2212d2

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -9,11 +9,12 @@ package com.xiaomi.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.display.DisplayManager;
 import android.os.IBinder;
 import android.os.UserHandle;
 import android.util.Log;
+import android.view.Display;
 import android.view.Display.HdrCapabilities;
-import android.view.SurfaceControl;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final String TAG = "XiaomiParts";
@@ -28,8 +29,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             Log.d(TAG, "Received boot completed intent");
 
         // Override HDR types to enable HDR
-        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
-        SurfaceControl.overrideHdrTypes(displayToken,
+        final DisplayManager displayManager = context.getSystemService(DisplayManager.class);
+        displayManager.overrideHdrTypes(Display.DEFAULT_DISPLAY,
                 new int[] {HdrCapabilities.HDR_TYPE_HDR10,HdrCapabilities.HDR_TYPE_HLG,
                         HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
