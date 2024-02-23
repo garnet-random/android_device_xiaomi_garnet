af38648b39619b8ccbc65a78b0a2437fe692441b
Jiyong Park <jiyong@google.com>
2024-02-24 05:24:40 +0530

    garnet: support updatable APEX
    Before, updatable_apex.mk was inherited indirectly from
    mainline_system.mk. Since mainline_system.mk no longer inherits
    updatable_apex.mk, updatable_apex.mk is inherited directly from
    device-specific mk file.
    
    Bug: 132664085
    Test: choosecombo to garnet; get_build_var TARGET_FLATTEN_APEX
    shows false
    
    Change-Id: I7162567b732f284f618a77cf09f5b40aadd913d5
    Merged-In: I7162567b732f284f618a77cf09f5b40aadd913d5

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
