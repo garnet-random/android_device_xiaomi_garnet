fdb5953b7730bd648ad94167f26a236b9f7188f9
AdarshGreal <adarshgrewal@gmail.com>
2024-02-24 04:56:51 +0530

    garnet: Include qcom vendor sepolicy
    Change-Id: I850b0101db00ae7b8483c8406df2adc25816038f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,6 +136,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Sepolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
