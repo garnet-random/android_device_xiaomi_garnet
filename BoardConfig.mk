7d64cb76a26b43cc69c9f1ff45404789daf1d670
AdarshGreal <adarshgrewal@gmail.com>
2024-02-24 11:35:54 +0530

    garnet: Add some Broken build rules
    Change-Id: Iabf94416d7ffe1f76e9851b760de8c50ed79a869

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/garnet
 
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := \
