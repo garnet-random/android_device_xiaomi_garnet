80ae1c6430b32d7e58f366cc7729929544508e54
AdarshGrewal <adarshgrewal@gmail.com>
2024-02-25 01:31:07 +0530

    garnet: Declare api level
    Change-Id: Iad69dd794ed17f8cecff5003336277ffa5da3d66

--- device.mk
+++ device.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# API
+BOARD_API_LEVEL := 31
+BOARD_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 31
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
