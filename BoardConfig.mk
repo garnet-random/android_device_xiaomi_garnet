fc3837a0b6338b4e306b1a3aae7c091f71862ed9
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-06 10:29:32 +0530

    garnet: Import bluetooth profile properties
    Change-Id: Ic13a243abf02efd009a7bec46f74528c9533e7e9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,6 +183,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 # System properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/props/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/props/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
