b9002d20a1c7d736131586a3f9ebc5ef42491b3c
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-13 03:53:20 +0530

    garnet: Build qti health service from source
    Change-Id: Ie1d264eec980ed88d0ac84f2e0980069b94a67f1

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1.vendor
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
