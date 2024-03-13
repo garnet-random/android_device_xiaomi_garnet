98afca02b602d295a259c008993c6d126abdeb31
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-13 14:09:51 +0530

    garnet: Build ir service from source
    Change-Id: I52037279617fb7c582fb18dd556bac7ae17f5409

--- device.mk
+++ device.mk
@@ -251,6 +251,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
