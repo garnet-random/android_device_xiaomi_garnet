7b29dc0e6d378c0cbfddcc5d70cf22ea29d91d9b
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-14 19:17:21 +0530

    garnet: Build bt components from source
    Change-Id: If2fc0fe1c78f8ad5f0713b29f9029d6caa053f41

--- device.mk
+++ device.mk
@@ -109,7 +109,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.audio@2.1.vendor
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
+PRODUCT_PACKAGES += \
+    com.dsi.ant@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
