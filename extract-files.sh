5df0244ed6e0f5ec5ec8eb1a0172e56bd6bea9df
Arian <arian.kulmer@web.de>
2024-03-19 23:13:00 +0100

    garnet: Add security.rkp-V3 dependency to keymint service
    The symbol _ZN4aidl7android8hardware8security7keymint29IRemotelyProvisionedComponent10descriptorE
    was moved from android.hardware.security.keymint-V2-ndk to android.hardware.security.rkp-V3-ndk.
    
    Change-Id: I7f3c40e3e8a2a848cdcb73e5232952f63681aa58

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/bin/hw/android.hardware.security.keymint-service-qti|vendor/lib/libqtikeymint.so|vendor/lib64/libqtikeymint.so)
+            grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || "${PATCHELF_0_17_2}" --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
+            ;;
         vendor/etc/camera/pureView_parameter.xml)
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
             ;;
