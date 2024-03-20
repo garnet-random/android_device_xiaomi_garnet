44b336d54c470ce293c2780ece7fc37ee9ee7482
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-20 13:37:17 +0530

    Revert "garnet: udfps: Take vendor code 20 into account"
    This reverts commit 98318b0025fdbf969daa386656e2f7e2c2f55495.
    
    Change-Id: Idcb7ece18dd7d4fd1afb502e19b925384bab7651

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -122,7 +122,7 @@ class XiaomiGarnetUdfpsHander : public UdfpsHandler {
         if (result == FINGERPRINT_ACQUIRED_GOOD) {
             setFingerDown(false);
             setFodStatus(FOD_STATUS_OFF);
-        } else if (vendorCode == 20 || vendorCode == 21 || vendorCode == 23) {
+        } else if (vendorCode == 21 || vendorCode == 23) {
             /*
              * vendorCode = 21 waiting for fingerprint authentication
              * vendorCode = 23 waiting for fingerprint enroll
