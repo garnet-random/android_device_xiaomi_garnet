e7dff28c16cb337221f4b1aef52d779dc5ee57e3
AdarshGrewal <adarshgrewal@gmail.com>
2024-04-17 00:02:24 +0530

    garnet: Build remaining gnss interfaces from source
    Change-Id: I48acac8a76ec06ccf8d40850fbd257b2f87a7cf5

--- device.mk
+++ device.mk
@@ -201,8 +201,10 @@ PRODUCT_COPY_FILES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.1.vendor \
-    android.hardware.gnss-V1-ndk_platform.vendor
+    android.hardware.gnss-V1-ndk_platform.vendor \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
