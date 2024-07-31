64d815995cc29b6a3ae608f7159c038a9f3c1837
AdarshGrewal <adarshgrewal@gmail.com>
2024-07-31 12:25:55 +0530

    garnet: Set FOD status on before polling FOD_PRESS_STATUS_PATH
     * so fp on garnet is a mess
     - goodix fp + goodix touch devices returns vendor code 21 when waiting for fp
     - fpc fp + goodix touch devices returns vendor code 22 when waiting for fp
     - fpc fp + focaltech touch devices returns vendor code 0 or nothing at times when waiting for fp
    
     this is more a hack than anything will deal with it when we move ot oss kernel
    
    Change-Id: Ic6645bdfcc699477d6bf661a5d6cf7adca41a18b

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -89,6 +89,8 @@ class XiaomiGarnetUdfpsHander : public UdfpsHandler {
         touch_fd_ = android::base::unique_fd(open(TOUCH_DEV_PATH, O_RDWR));
         disp_fd_ = android::base::unique_fd(open(DISP_FEATURE_PATH, O_RDWR));
 
+        setFodStatus(FOD_STATUS_ON);
+
         // Thread to notify fingeprint hwmodule about fod presses
         std::thread([this]() {
             int fd = open(FOD_PRESS_STATUS_PATH, O_RDONLY);
@@ -187,7 +189,6 @@ class XiaomiGarnetUdfpsHander : public UdfpsHandler {
 
     void cancel() {
         LOG(INFO) << __func__;
-        setFodStatus(FOD_STATUS_OFF);
         setFingerDown(false);
     }
 
