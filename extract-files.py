52e15ccfb293e5321bc87df662919b0bcb076811
LuK1337 <priv.luk@gmail.com>
2025-07-07 15:56:40 +0200

    garnet: Make libwfdservice use android.hardware.common-V2-ndk
    See: https://review.lineageos.org/c/436647
    Change-Id: I1afb36259421f1f4360dc723a187c31786edaffc

--- extract-files.py
+++ extract-files.py
@@ -61,6 +61,10 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('android.hidl.base@1.0.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed(
+            'android.hardware.common-V2-ndk_platform.so',
+            'android.hardware.common-V2-ndk.so'
+        )
+        .replace_needed(
             'android.media.audio.common.types-V2-cpp.so',
             'android.media.audio.common.types-V4-cpp.so'
         ),
