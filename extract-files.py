d9ca615cd7c8748003c00c6f59d21f9cb176dc04
kleidione Freitas <kleidione@gmail.com>
2025-12-22 08:43:31 -0300

    garnet: Rollback and pin video filter LUTs and libraries to OS2.0.11.0.VNRMIXM
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -98,7 +98,8 @@ blob_fixups: blob_fixups_user_type = {
     (
         'vendor/lib64/libalhLDC.so',
         'vendor/lib64/libalLDC.so',
-        'vendor/lib64/libTrueSight.so'
+        'vendor/lib64/libTrueSight.so',
+        'vendor/lib64/libMiVideoFilter.so'
     ): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
