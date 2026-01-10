b5bd46b58fdd80c53aff7aca952eda1324c752d8
Michael Bestas <mkbestas@lineageos.org>
2026-01-10 16:03:18 -0300

    garnet: Patch qccsyshal to load versioned libprotobuf
    Change-Id: I9ec95bf55afd62a03b2bd787c8b82c15b921a452

--- extract-files.py
+++ extract-files.py
@@ -131,6 +131,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('android.hardware.security.rkp-V1-ndk.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
