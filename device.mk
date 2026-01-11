63ec37072d7b3ac186aed60816c2fb669a33847c
kleidione Freitas <kleidione@gmail.com>
2026-01-11 07:48:25 -0300

    garnet: Align keylayout installation path with expected location
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -233,7 +233,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/parrot-qrd-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/usr/keylayout/parrot-qrd-snd-card_Button_Jack.kl
+    $(LOCAL_PATH)/configs/keylayout/parrot-qrd-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/parrot-qrd-snd-card_Button_Jack.kl
 
 # Keymint
 PRODUCT_COPY_FILES += \
